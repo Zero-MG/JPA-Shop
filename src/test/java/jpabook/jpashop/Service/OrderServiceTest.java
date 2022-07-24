e30fdbab87a292d6657f1515ca6ff7334cbb7f6b
Zero-MG <wintersia00@gmail.com>
2022-07-24 23:34:01 +0900

    220724 OrderService TEST Code

--- src/test/java/jpabook/jpashop/Service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/Service/OrderServiceTest.java
@@ -6,6 +6,7 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.exception.NotEnoughStockException;
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.OrderRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -34,16 +35,9 @@ public class OrderServiceTest {
     @Test
     public void 상품주문() throws Exception {
         //이런게 주어졌을 때 :: given
-        Member member = new Member();
-        member.setName("회원1");
-        member.setAddress(new Address("서울", "강가", "123-123"));
-        em.persist(member);
+        Member member = createMember();
 
-        Book book = new Book();
-        book.setName("시골 JPA");
-        book.setPrice(10000);
-        book.setStockQuantity(10);
-        em.persist(book);
+        Book book = createBook("시골 JPA", 10000, 10);
 
         int orderCount = 2;
 
@@ -59,6 +53,23 @@ public class OrderServiceTest {
         assertEquals("주문 수량만큼 재고가 줄어야 한다.", 8, book.getStockQuantity());
     }
 
+    private Book createBook(String name, int price, int stockQuantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        em.persist(book);
+        return book;
+    }
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "강가", "123-123"));
+        em.persist(member);
+        return member;
+    }
+
     @Test
     public void 주문취소() throws Exception {
         //이런게 주어졌을 때 :: given
@@ -72,6 +83,8 @@ public class OrderServiceTest {
     @Test(expected = NotEnoughStockException.class)
     public void 상품주문_재고수량초과() throws Exception {
         //이런게 주어졌을 때 :: given
+        Member member = createMember();
+        Item item = createBook("시골 JPA", 10000, 10);
 
         //이걸 실행하면 :: when
 
