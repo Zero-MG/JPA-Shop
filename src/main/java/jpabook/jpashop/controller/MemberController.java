e2b82ed0695f9530f07c9617d6779f864352f965
Zero-MG <wintersia00@gmail.com>
2022-07-28 23:01:42 +0900

    220728 회원가입 구현, Valid & BindingResult & NotEmpty(message="") 사용

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -1,9 +1,16 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.Service.MemberService;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -11,8 +18,32 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @GetMapping("/members/new")
     public String creatForm(Model model) {
-        return "";
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String creat(@Valid MemberForm form, BindingResult result) {
+
+        /**
+         * '@Valid'과 'BindingResult'를 같이 써주면
+         * 아래 처럼 오류시 페이지 처리를 할 수 있다.
+         * 스프링과 타임리프는 인티그레이션이 강하게 되어있음.
+         */
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
     }
 
 }
--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -8,8 +8,9 @@ import javax.validation.constraints.NotEmpty;
 @Getter @Setter
 public class MemberForm {
 
-    @NotEmpty(message = "회원 이름은 필수 입니다.")
+    @NotEmpty(message = "회원 이름은 필수 입니다")
     private String name;
+
     private String city;
     private String street;
     private String zipcode;
