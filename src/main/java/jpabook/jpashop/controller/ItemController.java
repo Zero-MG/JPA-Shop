5bdc7f93b1278717861f8bdd01196cae01a48f53
Zero-MG <wintersia00@gmail.com>
2022-08-04 21:59:18 +0900

    220804 - 병합(merge)과 변경감지에 대한 리팩토링

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -62,21 +62,43 @@ public class ItemController {
     }
 
     @PostMapping("items/{itemId}/edit")
-    public String updateItem (@ModelAttribute("form") BookForm form) {
+    public String updateItem (@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
         /**
          * 이 경우 id값을 url 파라미터로 받기 때문에 변형가능성이 높다.
          * 그러므로 수정하는 사람의 권한체크를 하는 것이 좋다.
          * 예) '작성자 == 수정자' 체크
          */
-        Book book = new Book();
-        book.setId(form.getId());
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
+//        Book book = new Book();
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
+//        itemService.saveItem(book);
+        /** 이 경우는 "어설프게 Controller에서 Entity를 생성하지 마라"라는 어원 담아,
+         * 개선한 코드로 리팩토링 하였습니다.
+         */
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
+        // 변경감지 형식 및 코드 약간 간소화
+        // DTO 같은 것을 하나 만들어서 해주는 것이 더 간소화 될 수 있어서 좋음
 
-        itemService.saveItem(book);
+        /** 민구의 머지와 변경감지 총 정리 :: Update 주의사항 **
+         * 
+         * 우선 위와같이 줄였지만, 중요한 요점은 Update 시,
+         * 병합(merge)를 쓰지말고, JPA 변경감지를 쓰자!
+         *
+         * 병합은 모든 데이터를 전부 통째로 바꿔버림 :: 원하는 곳만 수정이 불가능함.
+         *
+         * 변경감지는 '영속성 데이터'를 set으로 변경할 시
+         * JPA가 감지하여 수정된 것을 자동으로 알아차린 뒤 감지한 곳만 데이터를 변경해줌.
+         *
+         * '영속성'이라는 것을 설명에 넣은 이유는 영속성 컨텍스트에 없다면, JPA가 관리하지 않기때문에
+         * 변경감지 또한 알아차릴 수 없다. 무슨말이냐면, JPA는 영속성 컨텍스트 안에서 관리되기 때문에,
+         * 영속성 컨텍스트 밖에서 변경된 값은 볼 수가 없어서 모른다는 것이다.
+         * 이러함으로 인해, 변경 되었는지 또한 알 수가 없는 것이다.
+         */
+
         return "redirect:/items";
     }
 
